f723251316ea59a85c4bc79d19e86d36e01edb74
pnkowalczuk <pnk8000@rit.edu>
2018-05-08 15:50:27 -0400

    changed the token to be a singleton class

--- lib/tokenwrapper.rb
+++ lib/tokenwrapper.rb
@@ -5,37 +5,51 @@ require 'json'
 require 'date'
 
 module Tokenwrapper
-  def Tokenwrapper.getToken	
-    uri = URI.parse("http://todoable.teachable.tech/api/authenticate")
-    request = Net::HTTP::Post.new(uri)
-    request.basic_auth("pnk8000@rit.edu", "todoable")
-    request.content_type = "application/json"
-    request["Accept"] = "application/json"
-
-    req_options = {
-      use_ssl: uri.scheme == "https",
-    }
-
-    response = Net::HTTP.start(uri.hostname, uri.port, req_options) do |http|
-      http.request(request)
-    end
-	if response.code.eql?("200")
-      t = Token.new(response)
-	end
-	t
+  def Tokenwrapper.getToken
+	t = Token.getInstance
   end
-  
   class Token
-    def initialize(apiResponse)
-      tempHash = JSON.parse(apiResponse.body)
+    $instance = nil
+	def initialize  	 
+      uri = URI.parse("http://todoable.teachable.tech/api/authenticate")
+      request = Net::HTTP::Post.new(uri)
+      request.basic_auth("pnk8000@rit.edu", "todoable")
+      request.content_type = "application/json"
+      request["Accept"] = "application/json"
+
+      req_options = {
+        use_ssl: uri.scheme == "https",
+      }
+
+      response = Net::HTTP.start(uri.hostname, uri.port, req_options) do |http|
+        http.request(request)
+      end
+      tempHash = JSON.parse(response.body)
 	  @tcode = tempHash["token"]
-	  @texpiration = DateTime.parse(tempHash["expires_at"])
+	  @texpiration = DateTime.parse(tempHash["expires_at"]).to_time.utc
 	end
+	
+	def self.getInstance
+	  if $instance == nil
+		$instance = Token.new
+	  elsif $instance.expiresSoon
+		$instance = Token.new
+	  end 
+		$instance
+	end
+
 	def getCode
 	  @tcode
 	end
+
 	def getExpireTime
 	  @texpiration
 	end
+    
+	def expiresSoon
+	  (getExpireTime - Time.now.utc) < 60
+	end
   end
 end
+puts Tokenwrapper.getToken.getCode
+puts Tokenwrapper.getToken.getCode
