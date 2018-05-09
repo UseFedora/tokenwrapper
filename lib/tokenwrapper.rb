e1cdd6dfcc4c1347d25e8657e417ea8010e792c1
pnkowalczuk <pnk8000@rit.edu>
2018-05-08 22:18:01 -0400

    500 error, but I think post list works

--- lib/tokenwrapper.rb
+++ lib/tokenwrapper.rb
@@ -8,6 +8,72 @@ module Tokenwrapper
   def Tokenwrapper.getToken
 	t = Token.getInstance
   end
+  
+  def Tokenwrapper.getAllLists
+    t = Tokenwrapper.getToken
+	
+	uri = URI.parse("http://todoable.teachable.tech/api/lists")
+	request = Net::HTTP::Get.new(uri)
+	request.content_type = "application/json"
+	request["Authorization"] = "Token token=\"%s\"" % t.getCode
+	request["Accept"] = "application/json"
+
+	req_options = {
+	  use_ssl: uri.scheme == "https",
+	}
+
+	response = Net::HTTP.start(uri.hostname, uri.port, req_options) do |http|
+	  http.request(request)
+	end
+	tempHash = JSON.parse(response.body)
+	tempHash["lists"]
+	puts response.body
+  end
+
+  def Tokenwrapper.postList(listName)
+    t = Tokenwrapper.getToken
+	uri = URI.parse("http://todoable.teachable.tech/api/lists")
+	request = Net::HTTP::Post.new(uri)
+	request.content_type = "application/json"
+	request["Authorization"] = "Token token=\"%s\"" % t.getCode
+	request["Accept"] = "application/json"
+	request.body = "list {
+						   name: %s
+						 }
+					}" %listName
+	
+	req_options = {
+	  use_ssl: uri.scheme == "https",
+	}
+
+    response = Net::HTTP.start(uri.hostname, uri.port, req_options) do |http|
+	  http.request(request)
+	end
+    response.code == "201"
+  end
+
+  def getList(id)
+    
+    t = Tokenwrapper.getToken
+	
+	uri = URI.parse("http://todoable.teachable.tech/api/lists/%d" %id)
+	request = Net::HTTP::Get.new(uri)
+	request.content_type = "application/json"
+	request["Authorization"] = "Token token=\"%s\"" % t.getCode
+	request["Accept"] = "application/json"
+
+	req_options = {
+	  use_ssl: uri.scheme == "https",
+	}
+
+	response = Net::HTTP.start(uri.hostname, uri.port, req_options) do |http|
+	  http.request(request)
+	end
+	tempHash = JSON.parse(response.body)
+	tempHash["lists"]
+	puts response.body
+  end
+
   class Token
     $instance = nil
 	def initialize  	 
@@ -32,13 +98,14 @@ module Tokenwrapper
 	def self.getInstance
 	  if $instance == nil
 		$instance = Token.new
-	  elsif $instance.expiresSoon
+	  elsif $instance.expiresSoon?
 		$instance = Token.new
 	  end 
 		$instance
 	end
 
 	def getCode
+	  refreshIfExpiresSoon
 	  @tcode
 	end
 
@@ -46,8 +113,14 @@ module Tokenwrapper
 	  @texpiration
 	end
     
-	def expiresSoon
+	def expiresSoon?
 	  (getExpireTime - Time.now.utc) < 60
+	end
+
+	def refreshIfExpiresSoon
+      if expiresSoon?
+		getInstance
+	  end
 	end
   end
 end
